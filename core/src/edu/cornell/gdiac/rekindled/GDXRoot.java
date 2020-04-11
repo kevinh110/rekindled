ede2aea75499ee8d530f7dcde107463445e4efc8
Calvin Woo <yw564@cornell.edu>
2020-04-11 17:10:30 -0400

    new level

--- core/src/edu/cornell/gdiac/rekindled/GDXRoot.java
+++ core/src/edu/cornell/gdiac/rekindled/GDXRoot.java
@@ -71,12 +71,13 @@ public class GDXRoot extends Game implements ScreenListener {
 		loading = new LoadingMode(canvas,manager,1);
 
 		// Initialize the three game worlds
-		controllers = new GameplayController[5];
+		controllers = new GameplayController[10];
 		controllers[0] = new GameplayController("jsons/level0.json");
 		controllers[1] = new GameplayController("jsons/level1.json");
 		controllers[2] = new GameplayController("jsons/level2.json");
 		controllers[3] = new GameplayController("jsons/level3.json");
 		controllers[4] = new GameplayController("jsons/level4.json");
+		controllers[9] = new GameplayController("jsons/levela.json");
 		for(int ii = 0; ii < controllers.length; ii++) {
 			controllers[ii].preLoadContent(manager);
 		}
